37b733e840dcd31d733aa862a70439c33b1404c7
DongZX <dongzhixi0321@qq.com>
2023-09-02 19:37:21 +0800

    初始化electron框架

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+#app{
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <div class="biliContainer">
+      <iframe class="webPageContainer" src="https://space.bilibili.com/9358935/favlist"></iframe>
+    </div>
+</template>
+
+<style scoped>
+.biliContainer{
+  height: 100%;
+  width: 100%;
+  background-color: aqua;
+}
+
+.webPageContainer{
+  width: 100%;
+  height: 100%;
+}
+</style>
